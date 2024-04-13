3aa221dbf4559a84b29462d0b23727af2e3020c1
Young L <yokimiyaz@gmail.com>
2024-04-13 22:32:16 +1000

    finish chatroom and dou-communication

--- Assignment/__init__.py
+++ Assignment/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_socketio import join_room, leave_room, send, SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '365eff69d1d3c150a10adc94f2926365'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
+socketio = SocketIO(app)
 bcrypt = Bcrypt(app) 
 login_manager = LoginManager(app)
+
 
 from Assignment import routes
