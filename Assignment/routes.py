cd862f25a29f7a8dc3938bdd13b12c76caffae51
Young L <yokimiyaz@gmail.com>
2024-04-16 19:58:58 +1000

    show_firend_request

--- Assignment/routes.py
+++ Assignment/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, url_for, flash, redirect, request, session, make_response
 from Assignment import app, db, bcrypt, socketio
 from Assignment.forms import Registeration, Login
-from Assignment.models import User, Message
+from Assignment.models import User, Message, Friend, FriendRequest
 from flask_login import login_user, current_user
-from flask_socketio import join_room, leave_room, send, SocketIO
+from flask_socketio import join_room, leave_room, send, emit
 from string import ascii_uppercase
 import random
 
@@ -26,30 +26,33 @@ def generate_unique_code(Length):
 def home():
     return render_template('home.html', title='homepage')
 
-# homepage after login
-@app.route("/chatroom", methods=["POST", "GET"])
+#homepage after login
+@app.route("/chathome", methods=["POST", "GET"])
 def chathome():
     current_user_name = request.cookies.get('username')
+    current_user = User.query.filter_by(username=current_user_name).first()
+    current_user_id= current_user.id
+    pending_requests = FriendRequest.query.filter_by(receiver_id=current_user_id, accepted=False).all()
+
     if request.method == "POST":
         code = request.form.get("code")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
         if  join!= False and not code:
-            return render_template('chathome.html', error='Please enter a Room code.', code = code, current_user_name=current_user_name)
+            return render_template('chathome.html', chat_error='Please enter a Room code.', current_user_name=current_user_name)
                    
         room = code
         if create != False:
             room = generate_unique_code(4)
             rooms[room] = {"members":0, "message":[]}
         elif code not in rooms:
-           return render_template('chathome.html', error='Room dose not exist.', code = code, current_user_name=current_user_name)
+           return render_template('chathome.html', chat_error='Room dose not exist.', current_user_name=current_user_name)
         
         session["room"] = room
         session["name"] = current_user_name
         return redirect(url_for("room"))
-
-
-    return render_template('chathome.html', title='chatroom', current_user_name=current_user_name)
+    
+    return render_template('chathome.html', title='chatroom', current_user_name=current_user_name, friend_requests=pending_requests)
 
 #register
 @app.route("/register", methods=['GET', 'POST'])
@@ -69,7 +72,7 @@ def register():
                 flash(f'Error in {getattr(form, field).label.text}: {error}', 'danger')
     return render_template('register.html', title='Register', form=form)
 
-###login
+#login
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = Login()
@@ -85,8 +88,39 @@ def login():
             
     return render_template('login.html', title='Login', form = form)
 
+
+# Socket listening for sending friend request event
+@socketio.on('send_friend_request')
+def send_friend_request(data):
+    friend_username = data.get('friendUsername')
+    sender_name = request.cookies.get('username')
+    if friend_username == sender_name:
+        emit('friend_request_error', {'error': 'You cannot add yourself as a friend.'})
+        return
+    
+    receiver = User.query.filter_by(username=friend_username).first()
+    if not receiver:
+        emit('friend_request_error', {'error': 'This user does not exist.'})
+        return
+    sender = User.query.filter_by(username=sender_name).first()
+    if friend_username in [friend.username for friend in current_user.friends]:
+        emit('friend_request_error', {'error': 'This user has already been your friend.'})
+        return
+    
+    repeat = FriendRequest.query.filter_by(receiver_id=receiver.id, sender_id=sender.id).first()
+    if repeat == None:
+        friend_request = FriendRequest(sender_id=sender.id, receiver_id=receiver.id)
+        db.session.add(friend_request)
+        db.session.commit()
+        emit('friend_request_notification', {'receiver': receiver.username})
+    else:
+        emit('friend_request_error', {'error': 'You already send the friend request.'})
+
+
+
+
 # chatroom
-@app.route("/room")
+@app.route("/chatroom")
 def room():
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
@@ -138,4 +172,4 @@ def message(data):
     send(content, to=room)
     # save message for history
     rooms[room]["message"].append(content)
-    print(f"{session.get("name")} said: {data['data']}")
+    print(f"{session.get('name')} said: {data['data']}")
