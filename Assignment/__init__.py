f2459b21322b0b1511fa6d1b1d800e1182a07db8
Young L <yokimiyaz@gmail.com>
2024-05-11 14:12:14 +1000

    css style, user role, remove friend, etc

--- Assignment/__init__.py
+++ Assignment/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_socketio import join_room, leave_room, send, SocketIO
 
 app = Flask(__name__)
+app.static_folder = 'static'
 app.config['SECRET_KEY'] = '365eff69d1d3c150a10adc94f2926365'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
