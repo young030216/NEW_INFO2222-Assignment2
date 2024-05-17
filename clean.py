5b7dfc23678c92be03a4ef39640adf7e99d03920
Junyu Zhou <jzho8728@uni.sydney.edu.au>
2024-05-17 22:06:31 +1000

    before the comment

--- clean.py
+++ clean.py
@@ -1,5 +1,11 @@
 from Assignment import db,app
+from Assignment.models import User, UserRole
 with app.app_context():
     db.drop_all()
     db.create_all()
+    user1 = User(username = "Sherlock", password = "2003", role = UserRole.admin)
+    user2 = User(username = "Young", password = "123", role = UserRole.admin)
+    db.session.add(user1)
+    db.session.add(user2)
     db.session.commit()
+    
